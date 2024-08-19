c9f9c075780f0ea36f580772fac9ae92d4189697
Msweli03 <thabanimsweli03@gmail.com>
2024-08-19 14:56:52 +0200

    login redirect

--- EventSystem/Models/Event.cs
+++ EventSystem/Models/Event.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime Date { get; set; }
+
         public int TotalSeats { get; set; }
         public int SeatsAvailable { get; set; }
         public ICollection<Registration> Registrations { get; set; }
--- EventSystem/Models/Registration.cs
+++ EventSystem/Models/Registration.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace EventSystem.Models
 {
@@ -10,6 +11,9 @@ namespace EventSystem.Models
         public string UserId { get; set; }
         public IdentityUser User { get; set; } 
         public string ReferenceNumber { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
         public string EmailAddress { get; set; }
        
     }
