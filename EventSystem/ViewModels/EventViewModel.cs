b1c3843ec56e83173307d2f4f3e2ac8befdc3e7d
Msweli03 <thabanimsweli03@gmail.com>
2024-08-19 16:54:14 +0200

    new

---
+++ EventSystem/ViewModels/EventViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace EventSystem.ViewModels
+{
+    public class EventViewModel
+    {
+        public int EventId { get; set; }
+
+        public string EventName { get; set; }
+
+        public string EventDescription { get; set; }
+
+        public DateTime EventDate { get; set; }
+        public int TotalSeats { get; set; }
+        public int SeatsAvailable { get; set; }
+    }
+}
